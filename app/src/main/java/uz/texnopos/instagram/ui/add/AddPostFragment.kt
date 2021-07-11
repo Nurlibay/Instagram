5232f0205db75f0d4c9dc8c46917d0a556abdc88
Nurlibay Koshkinbayev <koshkinbayevn@gmail.com>
2021-07-11 17:26:32 +0500

    Instagram app authentication finished !

---
+++ app/src/main/java/uz/texnopos/instagram/ui/add/AddPostFragment.kt
@@ -0,0 +1,7 @@
+package uz.texnopos.instagram.ui.add
+
+import androidx.fragment.app.Fragment
+import uz.texnopos.instagram.R
+
+class AddPostFragment: Fragment(R.layout.fragment_add_post) {
+}
---
+++ app/src/main/java/uz/texnopos/instagram/ui/search/SearchFragment.kt
@@ -0,0 +1,8 @@
+package uz.texnopos.instagram.ui.search
+
+import androidx.fragment.app.Fragment
+import uz.texnopos.instagram.R
+
+class SearchFragment: Fragment(R.layout.fragment_search) {
+
+}
